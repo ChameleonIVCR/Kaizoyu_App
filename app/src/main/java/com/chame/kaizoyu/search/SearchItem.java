7be83b6ba5fc77b54cd33da3f6f66f4d26afcb2b
Chame <chamycr@hotmail.com>
2021-08-23 13:44:14 -0500

    Start work on search pager

--- app/src/main/java/com/chame/kaizoyu/search/SearchItem.java
+++ app/src/main/java/com/chame/kaizoyu/search/SearchItem.java
@@ -1,7 +1,30 @@
 package com.chame.kaizoyu.search;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
+import com.chame.kaizolib.common.model.Result;
+import com.chame.kaizoyu.R;
 
 public class SearchItem {
-    public static View
+    public static View inflateSearchItem(LayoutInflater inflater, Result searchResult){
+        View item = inflater.inflate(R.layout.item_result, null);
+
+        TextView title = (TextView) item.findViewById(R.id.text_title);
+        title.setText(searchResult.getFilename());
+
+        TextView text_bot = (TextView) item.findViewById(R.id.text_bot);
+        text_bot.setText(searchResult.getBot());
+
+        TextView quality = (TextView) item.findViewById(R.id.text_quality);
+        quality.setText(searchResult.getQuality());
+
+        TextView format = (TextView) item.findViewById(R.id.text_format);
+        format.setText(searchResult.getFormat());
+
+        TextView size = (TextView) item.findViewById(R.id.text_size);
+        size.setText(searchResult.getSize());
+
+        return item;
+    }
 }
--- app/src/main/java/com/chame/kaizoyu/search/scrappers/NiblSearch.java
+++ app/src/main/java/com/chame/kaizoyu/search/scrappers/NiblSearch.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import com.chame.kaizolib.common.model.Result;
 import com.chame.kaizolib.nibl.Nibl;
 import com.chame.kaizoyu.R;
+import com.chame.kaizoyu.search.SearchItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,24 +52,8 @@ public class NiblSearch implements Runnable {
             if (Thread.currentThread().isInterrupted()) {
                 return;
             }
-            View item = inflater.inflate(R.layout.item_result, null);
 
-            TextView title = (TextView) item.findViewById(R.id.text_title);
-            title.setText(result.getFilename());
-
-            TextView text_bot = (TextView) item.findViewById(R.id.text_bot);
-            text_bot.setText(result.getBot());
-
-            TextView quality = (TextView) item.findViewById(R.id.text_quality);
-            quality.setText(result.getQuality());
-
-            TextView format = (TextView) item.findViewById(R.id.text_format);
-            format.setText(result.getFormat());
-
-            TextView size = (TextView) item.findViewById(R.id.text_size);
-            size.setText(result.getSize());
-
-            items.add(item);
+            items.add(SearchItem.inflateSearchItem(inflater, result));
         }
 
         if (Thread.currentThread().isInterrupted()) {
