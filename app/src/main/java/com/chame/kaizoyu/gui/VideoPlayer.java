2b50eb511d29a3564ef60afdc430f7a66966fe47
Chame <chamycr@hotmail.com>
2021-08-29 13:58:01 -0500

    VideoPlayer implemented

--- app/src/main/java/com/chame/kaizoyu/gui/VideoPlayer.java
+++ app/src/main/java/com/chame/kaizoyu/gui/VideoPlayer.java
@@ -1,187 +1,85 @@
 package com.chame.kaizoyu.gui;
 
-import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
+import android.view.*;
 import android.widget.MediaController;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.VideoView;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.ViewModelProvider;
 import com.chame.kaizolib.irc.DCCDownloader;
-import com.chame.kaizolib.irc.model.DCC;
-import com.chame.kaizoyu.MainActivity;
+import com.chame.kaizolib.irc.IrcClient;
 import com.chame.kaizoyu.R;
-import com.chame.kaizoyu.download.scrappers.IRCGet;
-import com.chame.kaizoyu.utils.ThreadingAssistant;
+import com.chame.kaizoyu.gui.modelholder.VideoDownloadHolder;
+
+import io.github.tonnyl.spark.Spark;
 
 import java.io.File;
-import java.util.concurrent.Future;
 
 public class VideoPlayer extends AppCompatActivity {
-    private ThreadingAssistant thAssistant;
-    private String videoName;
-    private String videoCommand;
+    private Spark spark;
+    private ProgressBar initialProgressBar;
+    private TextView downloadSpeed;
+
+    private VideoDownloadHolder videoDataHolder;
+
+    private boolean isPlaying = false;
     private VideoView videoView;
-    private DCCDownloader download;
     private File downloadFile;
-    private Future ircThread;
-    private Future dccThread;
-
-
-    private static final boolean AUTO_HIDE = true;
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = this::hide;
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            switch (motionEvent.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    if (AUTO_HIDE) {
-                        delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                    }
-                    break;
-                case MotionEvent.ACTION_UP:
-                    view.performClick();
-                    break;
-                default:
-                    break;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bundle extras = getIntent().getExtras();
         setContentView(R.layout.activity_video_player);
 
-        Bundle extras = getIntent().getExtras();
+        initialProgressBar = findViewById(R.id.initial_download_progress);
+        initialProgressBar.setIndeterminate(false);
+        initialProgressBar.setProgress(0);
+        initialProgressBar.setMax(11);
+        downloadSpeed = findViewById(R.id.download_speed);
 
-        videoCommand = extras.getString("vCommand");
-        videoName = extras.getString("vname");
-
-        //Configure player
-        videoView = (VideoView) findViewById(R.id.video_frame);
-        MediaController ctlr = new MediaController(this);
-        ctlr.setMediaPlayer(videoView);
-        videoView.setMediaController(ctlr);
-        videoView.requestFocus();
-
-        mVisible = true;
-
-        mContentView = findViewById(R.id.video_frame);
-        mControlsView = (View) ctlr;
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        thAssistant = MainActivity.getInstance().getThreadingAssistant();
-        getDCCHandler();
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-    }
-
-    @Override
-    public void onStop () {
-        super.onStop();
-        if (download != null){
-            download.stop();
-        } else if (ircThread != null && !ircThread.isDone()) {
-            ircThread.cancel(true);
-        } else if (dccThread != null && !dccThread.isDone()) {
-            dccThread.cancel(true);
-        } else if (downloadFile != null){
-            downloadFile.delete();
+        //Temporary value
+        downloadFile = getCacheDir();
+        if (savedInstanceState != null){
+            downloadFile = new File(savedInstanceState.getString("file"));
+            isPlaying = savedInstanceState.getBoolean("isPlaying");
+            if (isPlaying) startPlayer();
         }
-    }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        delayedHide(100);
-    }
+        videoDataHolder = new ViewModelProvider(this).get(VideoDownloadHolder.class);
+        if (!videoDataHolder.hasStarted()){
+            videoDataHolder.initialize(extras.getString("vCommand"), getCacheDir());
+        }
 
-    private void getDCCHandler(){
-        IRCGet irc = new IRCGet(videoCommand);
-        irc.setListener(new IRCGet.IRCListener() {
+        videoDataHolder.setIrcOnFailureListener(new IrcClient.IrcOnFailureListener() {
             @Override
-            public void onIRCFailed(Exception e) {
-
-            }
-
-            @Override
-            public void onIRCFinished(DCC dcc) {
-                startDownload(dcc);
+            public void onFailure(IrcClient.FailureCode f) {
+                //TODO Error handling
             }
         });
-        ircThread = thAssistant.submitToDownloadThread(irc);
-    }
-
-    private void startDownload(DCC dcc){
-        downloadFile = new File(getCacheDir(), dcc.getFilename());
-        download = new DCCDownloader(dcc, downloadFile);
-        download.setDCCDownloadListener(new DCCDownloader.DCCDownloadListener() {
+        videoDataHolder.setDccDownloadListener(new DCCDownloader.DCCDownloadListener() {
             @Override
-            public void onDownloadReadyToPlay(int progress, File downloadFile) {
-                startPlayer(downloadFile);
+            public void onDownloadReadyToPlay(int progress, File df) {
+                downloadFile = df;
+                runOnUiThread(() -> startPlayer());
             }
 
             @Override
             public void onProgress(int progress, String speed) {
-
+                if (isPlaying){
+                    //TODO update the widget
+                } else {
+                    runOnUiThread(() -> initialProgressBar.setProgress(progress));
+                    runOnUiThread(() -> downloadSpeed.setText(speed));
+                }
+                System.out.println(speed);
+                System.out.println(progress);
             }
 
             @Override
@@ -194,53 +92,95 @@ public class VideoPlayer extends AppCompatActivity {
 
             }
         });
-        dccThread = thAssistant.submitToDownloadThread(download::start);
+
+        ConstraintLayout background = findViewById(R.id.video_background);
+
+        spark = new Spark.Builder()
+                .setView(background)   // set the layout of main screen
+                .setDuration(4000) // set duration
+                .setAnimList(Spark.ANIM_BLUE_PURPLE)  // set the color to change
+                .build();
+
+        //Configure player
+        videoView = (VideoView) findViewById(R.id.video_frame);
+        MediaController ctlr = new MediaController(this);
+        ctlr.setMediaPlayer(videoView);
+        videoView.setMediaController(ctlr);
+        videoView.requestFocus();
+        videoView.setVisibility(View.INVISIBLE);
+
+        if (getSupportActionBar() != null) getSupportActionBar().hide();
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+
+        videoDataHolder.start();
     }
 
-    private void startPlayer(File downloadFile){
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!isPlaying) spark.startAnimation(); // start animation on resume
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (!isPlaying) spark.stopAnimation(); // stop animation on pause
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        hideSystemUI();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("isPlaying", isPlaying);
+        outState.putString("file", downloadFile.getAbsolutePath());
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController controller = getWindow().getInsetsController();
+
+            if (controller != null)
+                controller.hide(WindowInsets.Type.statusBars());
+        }
+        else {
+            //noinspection deprecation
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        }
+    }
+
+    private void startPlayer(){
+        spark.stopAnimation();
+        ConstraintLayout background = findViewById(R.id.video_background);
+        background.setBackgroundColor(Color.BLACK);
+        initialProgressBar.setVisibility(View.GONE);
+        downloadSpeed.setVisibility(View.GONE);
+
+        isPlaying = true;
+        videoView.setVisibility(View.VISIBLE);
         videoView.setVideoPath(String.valueOf(Uri.fromFile(downloadFile)));
         videoView.start();
-    }
-
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in delay milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 }
---
+++ app/src/main/java/com/chame/kaizoyu/gui/modelholder/VideoDownloadHolder.java
@@ -0,0 +1,89 @@
+package com.chame.kaizoyu.gui.modelholder;
+
+import androidx.lifecycle.ViewModel;
+import com.chame.kaizolib.irc.DCCDownloader;
+import com.chame.kaizolib.irc.IrcClient;
+import com.chame.kaizolib.irc.model.DCC;
+import com.chame.kaizoyu.MainActivity;
+import com.chame.kaizoyu.utils.ThreadingAssistant;
+
+import java.io.File;
+import java.util.concurrent.Future;
+
+public class VideoDownloadHolder extends ViewModel {
+    private ThreadingAssistant thAssistant = MainActivity.getInstance().getThreadingAssistant();
+    private Future ircFuture;
+    private Future dccFuture;
+
+    private IrcClient.IrcOnFailureListener onFailureListener;
+    private DCCDownloader.DCCDownloadListener dccListener;
+
+    private File cachePath;
+    private File downloadFile;
+
+    private IrcClient irc;
+    private DCCDownloader download;
+
+    private boolean hasStarted = false;
+
+    public void initialize(String videoCommand, File cachePath){
+        final String username = MainActivity.getInstance().getConfigManager().getProperty("ircName");
+        this.irc = new IrcClient(videoCommand, username);
+        this.cachePath = cachePath;
+    }
+
+    public void setIrcOnFailureListener(IrcClient.IrcOnFailureListener onFailureListener){
+        this.onFailureListener = onFailureListener;
+        if (irc != null){
+            irc.setIrcOnFailureListener(onFailureListener);
+        }
+    }
+
+    public void setDccDownloadListener(DCCDownloader.DCCDownloadListener dccListener){
+        this.dccListener = dccListener;
+        if (download != null){
+            download.setDCCDownloadListener(dccListener);
+        }
+    }
+
+    public boolean hasStarted(){
+        return hasStarted;
+    }
+
+    public void start(){
+        if (hasStarted) return;
+        irc.setIrcOnFailureListener(onFailureListener);
+        irc.setIrcOnSuccessListener(this::startDownload);
+        hasStarted = true;
+        ircFuture = thAssistant.submitToDownloadThread(irc::execute);
+    }
+
+    @Override
+    public void onCleared(){
+        stop();
+        super.onCleared();
+    }
+
+    public void stop(){
+        onFailureListener = null;
+        dccListener = null;
+
+        if (download != null){
+            download.stop();
+        } else if (ircFuture != null) {
+            ircFuture.cancel(true);
+        } else if (dccFuture != null) {
+            dccFuture.cancel(true);
+        } else if (downloadFile != null && downloadFile.isFile()){
+            //TODO if delete is not successfully completed, schedule cache cleaning at app close
+            downloadFile.delete();
+        }
+    }
+
+    private void startDownload(DCC dcc){
+        downloadFile = new File(cachePath, dcc.getFilename());
+        download = new DCCDownloader(dcc, downloadFile);
+        download.setDCCDownloadListener(dccListener);
+        dccFuture = thAssistant.submitToDownloadThread(download::start);
+    }
+}
