b0893b8feb769dbd7305c8880aa2943ae23df40b
Chame <chamycr@hotmail.com>
2021-09-06 21:09:42 -0500

    Introduce SettingsFragment and prettified XML layouts

--- app/src/main/java/com/chame/kaizoyu/gui/HomeFragment.java
+++ app/src/main/java/com/chame/kaizoyu/gui/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.chame.kaizoyu.gui;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -47,7 +48,7 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
 
     }
 }
